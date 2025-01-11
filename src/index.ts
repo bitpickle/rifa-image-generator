730aebb885c4d9db0433aaa35a08055a2d712715
Péricles Pires <periclespires@protonmail.com>
2025-01-11 19:16:35 -0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,36 @@
-import { Elysia } from "elysia";
+import swagger from "@elysiajs/swagger";
+import { Elysia, t } from "elysia";
+import {
+  generateImageHtml,
+  htmlToImage,
+  saveImageOnMinio,
+} from "./rifas/image.generator";
+import staticPlugin from "@elysiajs/static";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(swagger())
+  .use(staticPlugin())
+  .post(
+    "/",
+    async ({ body, set }) => {
+      const { numeros, totalBilhetes, nomeRifa } = body;
+      const html = generateImageHtml(numeros, totalBilhetes, nomeRifa);
+      const image = await htmlToImage(html);
+      const url = await saveImageOnMinio(image);
+      console.log(url);
+      return {
+        url,
+      };
+    },
+    {
+      body: t.Object({
+        numeros: t.Array(t.Number()),
+        totalBilhetes: t.Number(),
+        nomeRifa: t.String(),
+      }),
+    }
+  )
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
---
+++ src/rifas/image.generator.ts
@@ -0,0 +1,105 @@
+import puppeteer from "puppeteer";
+import { Client } from "minio";
+import { uuidv7 } from "uuidv7";
+
+export function generateImageHtml(
+  soldNumbers: number[],
+  totalAmount: number,
+  name: string
+): string {
+  return `
+    <html>
+      <head>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            background-color: #f7f7f7;
+            margin: 0;
+            padding: 20px;
+          }
+          .container {
+            max-width: 800px;
+            margin: 0 auto;
+            text-align: center;
+          }
+          h1 {
+            color: #333;
+          }
+          table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+          }
+          td {
+            width: 10%;
+            padding: 10px;
+            border: 1px solid #ddd;
+            text-align: center;
+            font-size: 16px;
+            background-color: #fff;
+          }
+          td.sold {
+            background-color: #4caf50;
+            color: #fff;
+            font-weight: bold;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <h1>Tabela da Rifa ${name}</h1>
+          <table>
+            <tr>
+              ${Array.from({ length: totalAmount }, (_, index) => {
+                const number = index + 1;
+                const isSold = soldNumbers.includes(number);
+                return (
+                  `<td class="${isSold ? "sold" : ""}">${number}</td>` +
+                  ((index + 1) % 10 === 0 ? "</tr><tr>" : "")
+                );
+              }).join("")}
+            </tr>
+          </table>
+        </div>
+      </body>
+    </html>
+  `;
+}
+
+export async function htmlToImage(html = "") {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.setContent(html);
+
+  const content = await page.$("body");
+  const imageBuffer = await content!.screenshot({ omitBackground: true });
+
+  await page.close();
+  await browser.close();
+
+  return imageBuffer;
+}
+
+export async function saveImageOnMinio(
+  imageBuffer: Uint8Array<ArrayBufferLike>
+) {
+  const minio = new Client({
+    endPoint: "minio-api.bitpickle.dev",
+    port: 443,
+    useSSL: true,
+    accessKey: "lXp3uovgW428LraywbBU",
+    secretKey: "9G5GkHBYgVy6FTfb8VSEoPCtgmP2kbpAaL9IZcdV",
+  });
+
+  const uuid = uuidv7() + ".png";
+
+  await minio.putObject("dev", uuid, Buffer.from(imageBuffer), undefined, {
+    "content-type": "image/png",
+    "content-disposition": "inline",
+  });
+  return minio.presignedGetObject("dev", uuid, 60 * 60 * 24, {
+    "response-content-disposition": "inline",
+    "response-content-type": "image/png",
+  });
+}
